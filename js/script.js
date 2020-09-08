54a67064a2f42a26a9184dd0bf486bf24f019603
siddhudinde <56356213+siddhudinde@users.noreply.github.com>
2020-09-08 17:12:24 +0530

    Javascript

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+        
+        var w = Number(prompt("enter your weight in kg"));
+        var h = Number(prompt("enter your height in meter"));
+        var bmi = w / (h*h)
+        
+        if(bmi<18)
+        {
+            alert("your BMI is "+bmi+" you are underweigth");
+        }
+        else if(bmi>18 && bmi<25)
+        {
+            alert("your BMI is "+bmi+" you have normalweigth");
+        }
+        else
+        {
+            alert("your BMI is "+bmi+" you are underweigth");
+        }
+        
+        
+
+       
+      
+        
+        
---
+++ js/script2.js
@@ -0,0 +1,16 @@
+
+
+        var a = prompt ("Enter your full name")
+
+        a = a.toUpperCase();
+
+        var firstname = a.slice(0,6);
+        a = a.toLowerCase();
+
+        var lastname = a.slice(6,11);
+
+        alert("your Name is  "+firstname  +  lastname);
+
+        
+
+
